af1f8f2afc47f5cc3d563e56146baa7a2743d190
Stepami <Stepami@mail.ru>
2021-05-26 15:30:14 +0300

    Изменение модели данных

--- CourseWork.Lib/CWContext.cs
+++ CourseWork.Lib/CWContext.cs
@@ -66,7 +66,7 @@ namespace CourseWork.Lib
             modelBuilder.Entity<Trajectory>()
                 .HasOne(t => t.Specialization)
                 .WithMany()
-                .OnDelete(DeleteBehavior.SetNull);
+                .OnDelete(DeleteBehavior.Cascade);
 
             modelBuilder.Entity<TrajectoryElement>()
                 .HasOne(te => te.Course)
---
+++ CourseWork.Lib/Migrations/20210526122945_003.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseWork.Lib.Migrations
+{
+    public partial class _003 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trajectories_Specializations_SpecializationID",
+                table: "Trajectories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trajectories_Specializations_SpecializationID",
+                table: "Trajectories",
+                column: "SpecializationID",
+                principalTable: "Specializations",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trajectories_Specializations_SpecializationID",
+                table: "Trajectories");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trajectories_Specializations_SpecializationID",
+                table: "Trajectories",
+                column: "SpecializationID",
+                principalTable: "Specializations",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
