15e691c534d04eb0fb1e1aef4b68291efa7e3f7f
Stepami <Stepami@mail.ru>
2021-06-03 19:00:50 +0300

    Прохождение траектории

---
+++ CourseWork.API/Controllers/TrajectoriesController.cs
@@ -0,0 +1,33 @@
+﻿using CourseWork.Lib;
+using CourseWork.Lib.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseWork.API.Controllers
+{
+    public class TrajectoriesController : DbApiController
+    {
+        public TrajectoriesController(CWContext db) : base(db) { }
+
+        [Authorize]
+        [HttpPut]
+        [Route("{id}/pass")]
+        public async Task<TrajectoryElement> PassTrajectoryStep(Guid id)
+        {
+            var trajectory = await db.Trajectories.FindAsync(id);
+            var step = trajectory.Pass();
+            if (step != null)
+            {
+                db.Update(step);
+                await db.SaveChangesAsync();
+            }
+            return step;
+        }
+    }
+}
